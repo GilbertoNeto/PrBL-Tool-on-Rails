fc45d69556b1153a9d9363a4490d8a60204b068d
Gilberto Firmino de Souza Neto <gilbertofsneto@gmail.com>
2018-07-26 21:16:10 -0300

    Urgent update

--- prbl/config/environments/development.rb
+++ prbl/config/environments/development.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   address: 'smtp.gmail.com',
   port: 587,
   user_name: 'gilberto.neto@academico.ifpb.edu.br',
-  password: 'Ifpb120212',
+  password: '',
   authentication: 'plain',
   enable_starttls_auto: true
 }
